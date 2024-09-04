a29b6c44154e5867790b9fdb171e38ca9e12b087
Natalia <alamo.natalia@gmail.com>
2024-09-04 14:57:11 +0200

    styles test

---
+++ projects/ftn-components/src/lib/components/ftn-footer/ftn-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ftn-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './ftn-footer.component.html',
+  styles: ``
+})
+export class FtnFooterComponent {
+
+}
